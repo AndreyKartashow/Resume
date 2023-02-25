70db3f72cf9713efda4d19006fc67bdff394482c
Andrey <patron6239@gmail.com>
2023-02-25 12:00:16 +0300

    Adding JS

---
+++ index.js
@@ -0,0 +1,26 @@
+const navList = Array.from(document.querySelectorAll('.nav-button'))
+const contentList = Array.from(document.querySelectorAll('.content-container'))
+
+const clearActiveClass = (element) => {
+  element.find(item => item.classList.remove('selected'))
+}
+
+const setActiveClass = (element, index) => {
+  element[index].classList.add('selected')
+}
+
+const checkoutTabs = (item, index) => {
+  item.addEventListener('click', () => {
+    
+    if (item.classList.contains('selected')) return
+    console.log(item)
+  
+    clearActiveClass(navList)
+    clearActiveClass(contentList)
+    
+    setActiveClass(navList, index)
+    setActiveClass(contentList, index)
+  })
+}
+
+navList.forEach(checkoutTabs)
